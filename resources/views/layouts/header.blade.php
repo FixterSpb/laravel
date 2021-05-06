ddf7925095e8186d1e57ee1490a8fff756b79aec
Mladshev Evgeniy <fixter.spb@gmail.com>
2021-05-06 19:19:53 +0300

    Fixed some routes

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,8 +1,8 @@
 <header>
     <ul style="list-style-type: none" class="flex justify-center">
-        <li><a class="p-6" href="{{ url('/') }}">Главная</a></li>
+        <li><a class="p-6" href="{{ route('index.index') }}">Главная</a></li>
         <li><a class="p-6" href="{{ route('categories.index') }}">Категории</a></li>
-        <li><a class="p-6" href="{{ url('/about') }}">О проекте</a></li>
-        <li><a href="{{ route('auth.index') }}">Войти</a></li>
+        <li><a class="p-6" href="{{ route('about.index') }}">О проекте</a></li>
+        <li><a class="p-6" href="{{ route('auth.index') }}">Войти</a></li>
     </ul>
 </header>
