06eefa78cc1a7b5f9f14014374544e51d41e905a
Mladshev Evgeniy <fixter.spb@gmail.com>
2021-05-07 15:25:22 +0300

    Added tooltip to buttons.round

--- resources/views/components/floating-bar.blade.php
+++ resources/views/components/floating-bar.blade.php
@@ -1,5 +1,5 @@
 <div class="fixed right-8 bottom-8">
-    <x-buttons.round :route="route('news.create')">
-        {{ __('+') }}
+    <x-buttons.round :route="route('news.create')"  title="Добавить новость">
+        +
     </x-buttons.round>
 </div>
--- resources/views/news/create.blade.php
+++ resources/views/news/create.blade.php
@@ -13,14 +13,5 @@
             Добавление новости
         </x-text.title-h2>
         <x-news.news-create :categories="$categories" />
-{{--        <div class="flex flex-wrap justify-around">--}}
-{{--            @forelse($news as $newsItem)--}}
-{{--                <div class="w-full md:w-1/2 lg:w-1/3 px-3 mt-6">--}}
-{{--                    <x-news.news-preview :news="$newsItem" :categoryIsVisible="true"/>--}}
-{{--                </div>--}}
-{{--            @empty--}}
-{{--                <p class="text-center italic">Новостей пока нет</p>--}}
-{{--            @endforelse--}}
-{{--        </div>--}}
     </x-content-body>
 </x-app-layout>
