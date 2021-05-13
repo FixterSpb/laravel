077914072a89f4f8e5f0ded7d5a60fc08f36265e
Mladshev Evgeniy <fixter.spb@gmail.com>
2021-05-13 18:46:06 +0300

    Corrected the file config/files.php

--- config/files.php
+++ config/files.php
@@ -1,4 +1,10 @@
 <?php
 
-    const FILE_ABOUT_COMMENT_NAME = 'about-comments.txt';
-    const FILE_ORDERS_NAME = 'orders.txt';
+    if (!defined('FILE_ABOUT_COMMENT_NAME')){
+        define("FILE_ABOUT_COMMENT_NAME", 'about-comments.txt');
+    }
+
+    if (!defined('FILE_ORDERS_NAME')){
+        define("FILE_ORDERS_NAME", 'orders.txt');
+    }
+
