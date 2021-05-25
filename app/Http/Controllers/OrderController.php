1ef0acab1f517482bc5ee1c522ea74710d134656
Mladshev Evgeniy <fixter.spb@gmail.com>
2021-05-25 18:28:54 +0300

    Added model Order and migration for table orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\OrderStoreRequest;
+use App\Models\Order;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
@@ -12,20 +13,15 @@ class OrderController extends Controller
     public function store(OrderStoreRequest $request): \Illuminate\Http\RedirectResponse
     {
         $newOrder = [
-            'name' => $request->request->get('name'),
-            'phone' => $request->request->get('phone'),
-            'email' => $request->request->get('email'),
-            'description' => $request->request->get('description'),
-            'created_at' => now()
+            'name' => $request->get('name'),
+            'phone' => $request->get('phone'),
+            'email' => $request->get('email'),
+            'description' => $request->get('description'),
         ];
 
-        if (Storage::exists(FILE_ORDERS_NAME)){
-            $orders = json_decode(Storage::disk('local')->get(FILE_ORDERS_NAME));
-        }
+        $order = new Order($newOrder);
+        $order->save();
 
-        $orders[] = $newOrder;
-
-        Storage::put(FILE_ORDERS_NAME, json_encode($orders));
         return redirect()->route('dashboard')->with('success', 'Заказ успешно добавлен!');
     }
 }
